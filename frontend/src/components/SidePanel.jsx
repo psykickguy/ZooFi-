c02c45e6d2654874339b5aecac67595e8ce123df
PsyKickGuy <shubhdas2005@gmail.com>
2025-06-12 18:43:23 +0530

    last moment changes

--- frontend/src/components/SidePanel.jsx
+++ frontend/src/components/SidePanel.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Components.css";
-import BurningFooter from "../../react-burning-footer/index";
+// import BurningFooter from "../../react-burning-footer/index";
 
 export default function SidePanel() {
   return (
     <div className="side-panel">
       <div className="leftside">
-        <BurningFooter height={75} backgroundColor="#f0554d" />
+        {/* <BurningFooter height={75} backgroundColor="#f0554d" /> */}
       </div>
       <div className="rightside">
-        <BurningFooter height={75} backgroundColor="#f0554d" />
+        {/* <BurningFooter height={75} backgroundColor="#f0554d" /> */}
       </div>
     </div>
   );
